a1b48720837bb0e41ef6e23cdd967bc25275f703
carlosalfarodev <rgkalin@gmail.com>
2022-04-19 21:18:35 -0700

    connects Google Books API and make test call

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import Image from 'next/image';
 
 export default function index() {
+  const fetchBooks = async () => {
+    const books = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=${process.env.GOOGLE_BOOKS_API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+
+    console.log(books);
+  };
+  fetchBooks();
   return (
     <div>
       <h1>Welcome to Book Club, a work in progress...</h1>
