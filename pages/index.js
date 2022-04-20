4ff40f890b48af2b9ba95bdcd920820726740fe9
carlosalfarodev <rgkalin@gmail.com>
2022-04-19 20:46:42 -0700

    testing build to vercel

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function index() {
   return (
     <div>
-      <h1>Welcome to Book Club, a work in pro...</h1>
+      <h1>Welcome to Book Club, a work in progress...</h1>
     </div>
   );
 }
