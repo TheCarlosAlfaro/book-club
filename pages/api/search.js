7f10f28fac71366030faf2601950482e232b95d4
carlosalfarodev <rgkalin@gmail.com>
2022-04-24 12:57:50 -0700

    Adds API call from search for testing search component

---
+++ pages/api/search.js
@@ -0,0 +1,7 @@
+export default (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(
+    JSON.stringify({ results: [{ id: 2323, title: 'this is a result' }] })
+  );
+};
