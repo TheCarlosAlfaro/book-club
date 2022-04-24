49c4fb0278f5a2bcdf4b4ab2de388a75378c2fe0
carlosalfarodev <rgkalin@gmail.com>
2022-04-24 12:33:21 -0700

    Adds search page markup

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import Image from 'next/image';
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import Search from '../components/search';
+import utilStyles from '../styles/utils.module.css';
 
-export default function index() {
-  const fetchBooks = async () => {
-    const books = await fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=${process.env.GOOGLE_BOOKS_API_KEY}`
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-
-    console.log(books);
-  };
-  fetchBooks();
+export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Book Club, a work in progress...</h1>
-    </div>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Search</h2>
+        <Search />
+      </section>
+    </Layout>
   );
 }
