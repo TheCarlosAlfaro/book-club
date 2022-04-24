0e8644d99ac0ca8784e4ffc7b5ff06721af81c09
carlosalfarodev <rgkalin@gmail.com>
2022-04-24 15:33:02 -0700

    Adds Nextjs API testing endpoint

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,7 +1,5 @@
 export default (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.end(
-    JSON.stringify({ results: [{ id: 2323, title: 'this is a result' }] })
-  );
+  res.end(JSON.stringify({ results: [{ id: 2323, title: 'I love you ♥' }] }));
 };
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,21 @@ import Layout, { siteTitle } from '../components/layout';
 import Search from '../components/search';
 import utilStyles from '../styles/utils.module.css';
 
+export async function getStaticProps(props) {
+  console.log('hi getStaticProps');
+  console.log('google API KEY', process.env.GOOGLE_BOOKS_API_KEY);
+  const response = await fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=${process.env.GOOGLE_BOOKS_API_KEY}`
+  );
+
+  const data = await response.json();
+  console.log(data);
+
+  return {
+    props: data,
+  };
+}
+
 export default function Home() {
   return (
     <Layout home>
