6312f338c18b508de8cb6063b57ae7e26a750dff
carlosalfarodev <rgkalin@gmail.com>
2022-04-26 20:01:25 -0700

    Search functionality calling google api with results

--- pages/api/search.js
+++
@@ -1,5 +0,0 @@
-export default (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ results: [{ id: 2323, title: 'I love you ♥' }] }));
-};
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,31 @@
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import BookSearch from '../components/BookSearch';
-const googleBookApiKey = process.env.GOOGLE_BOOKS_API_KEY;
+import styles from '../components/search.module.css';
 
-export const getStaticProps = async () => {
-  const res = await fetch(
-    `https://www.googleapis.com/books/v1/volumes?q=javascript&key=${googleBookApiKey}`
-  );
-  const data = await res.json();
+export default function Home({ books, googleBooksApiKey }) {
+  const [formInputs, setFormInputs] = useState();
+  const [searchResults, setSearchResults] = useState([]);
 
-  return {
-    props: { books: data.items },
+  useEffect(() => {
+    setSearchResults([]);
+  }, [books]);
+
+  const handleInputs = (event) => {
+    let { name, value } = event.target;
+    setFormInputs({ ...formInputs, [name]: value });
   };
-};
 
-export default function Home({ books }) {
+  const search = async (event) => {
+    event.preventDefault();
+    let books = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${formInputs.searchTerm}&key=${googleBooksApiKey}`
+    );
+    books = await books.json();
+    setSearchResults(books.items);
+  };
+
   return (
     <Layout home>
       <Head>
@@ -23,9 +33,49 @@ export default function Home({ books }) {
       </Head>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Search</h2>
-        <BookSearch books={books} />
+
+        <div className={styles.container}>
+          <form onSubmit={search}>
+            <input
+              className={styles.search}
+              placeholder="Search by title or author"
+              name="searchTerm"
+              onChange={handleInputs}
+              type="text"
+              required
+            />
+            <button>Search</button>
+          </form>
+          {searchResults.length > 0 && (
+            <div className="giphy-search-results-grid">
+              {searchResults.map((each, index) => {
+                return (
+                  <div key={index}>
+                    <h3>{each.volumeInfo.title}</h3>
+                    {each.volumeInfo.imageLinks ? (
+                      <img
+                        src={each.volumeInfo.imageLinks.thumbnail}
+                        alt={each.title}
+                      />
+                    ) : (
+                      <p>no image found</p>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
       </section>
     </Layout>
   );
 }
-import React from 'react';
+
+export const getStaticProps = async () => {
+  const googleBooksApiKey = process.env.GOOGLE_BOOKS_API_KEY;
+  const books = {};
+
+  return {
+    props: { books, googleBooksApiKey },
+  };
+};
