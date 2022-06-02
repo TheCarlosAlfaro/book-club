a0a00cb527d86b6ea2dd96bbdf6a0995c4542485
carlosalfarodev <rgkalin@gmail.com>
2022-06-02 09:34:10 -0700

    Adds NextAuth with Github and Google providers

--- pages/index.js
+++ pages/index.js
@@ -3,28 +3,19 @@ import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Search from '../components/search';
+import Header from '../components/header';
+import { useSession } from 'next-auth/react';
 
 export default function Home() {
+  const { data: session } = useSession();
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <span>
-          <Link href="/signin">
-            <a>Sign In</a>
-          </Link>
-        </span>
-        {' - '}
-        <span>
-          <Link href="/signup">
-            <a>Sign Up</a>
-          </Link>
-        </span>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <Search />
+        <Header />
+        {session && <Search />}
       </section>
     </Layout>
   );
